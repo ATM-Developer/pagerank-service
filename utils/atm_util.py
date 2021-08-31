4c80fa9da689c19b6f0b9808f97b078e306aead7
beibei-deng <atm-pagerank-01@protagonist-ai.com>
2021-08-31 21:41:46 +0800

    add util

---
+++ utils/atm_util.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+class AtmUtil:
+
+    def __init__(self, url):
+        self._url = url
+
+    def get_coin_list(self):
+        res = requests.get(self._url + '/site/getCoinCurrencyList')
+        result = json.loads(res.text)
+        data_list = result.get('data', {}).get('coinCurrencyPairList', [])
+        coin_list = {}
+        for data in data_list:
+            symbol = data['baseCurrency']
+            coin_list[symbol] = {
+                'coefficient': data['coefficient'],
+                'nowPrice': data['nowPrice'],
+                'weiPlaces': int(data['weiPlaces']),
+                'priceByWei': data['nowPrice'] / 10 ** int(data['weiPlaces'])
+            }
+        return coin_list
